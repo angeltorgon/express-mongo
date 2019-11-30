7d31488aba7e8e8e6fd1cf9be5e32fb3b481226f
angel-torres <alas4224@gmail.com>
2019-11-30 17:45:43 -0600

    successfully connected to mongo database and was able to create a post endpoint

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ require('dotenv').config();
 const express = require('express');
 const server = express();
 const JournalRoute = require('./routes/journalRoute.js');
+const mongoose = require('mongoose');
 const port = process.env.PORT;
 
 server.listen(port, () => {
     console.log(`Listening on ${port}`)
 })
+
+mongoose.connect(process.env.DB_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
 
 server.use(express.json())
 
--- routes/journalRoute.js
+++ routes/journalRoute.js
@@ -6,4 +6,13 @@ JournalRoute.get('/', (req, res) => {
     res.send("You are in Journal route!")
 })
 
+JournalRoute.post('/', async (req, res) => {
+    const journalEntry = new Journal({
+        title: req.body.title,
+        body: req.body.body
+    })
+    const newEntry = await journalEntry.save();
+    res.send(newEntry)
+})
+
 module.exports = JournalRoute;
